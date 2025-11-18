bca9f5b623c5bac4cab2433b4eb316a7ad56033a
Rohan Garg <gargrohan138@gmail.com>
2025-11-18 10:28:11 +0530

    removing the old frontend project

--- frontend/src/components/fixedImage/fixedImage.jsx
+++
@@ -1,8 +0,0 @@
-import "../../output.css"
-export default function FixedImage() {
-  return (
-    <div className="h-max">
-      <div className="bg-fixed flex flex-col overflow-scroll bg-[url('/static/images/geometrhy.png')] bg-no-repeat max-w-full h-[450px] my-[5px] ml-[2px] bg-fixed object-scale-down"></div>
-    </div>
-  );
-}
