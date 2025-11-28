af27227be4c44e8a6bc5624dc8ddf33ef32770e6
rohan <gargrohan138@gmail.com>
2025-11-29 01:19:20 +0530

    added blur text animation and completed 1st iteration of home page

--- components/ui/Intro.tsx
+++ components/ui/Intro.tsx
@@ -1,24 +1,41 @@
+import BlurText from "./BlurText";
+
 export default function Intro() {
   return (
     <div className="2xl:h-full max-2xl:w-full 2xl:w-[700px] z-10 text-paper flex 2xl:flex-col 2xl:justify-end 2xl:sticky 2xl:top-0 ">
       <div className="flex justify-end">
         <div className="flex flex-col font-raleway max-2xl:mt-16 mx-8 mb-12">
-          <div className="font-medium mb-6 text-md">Rohan Garg</div>
-          <div className="font-normal text-md mb-6">
-            Full-Stack Web Developer / Start-Up Focussed Web Expert
+          <div className="font-medium mb-6 text-md">
+            <BlurText text="Rohan Garg" delay={150} direction="bottom" />
           </div>
           <div className="font-normal text-md mb-6">
-            IIT Roorkee 2024 Grauate
+            <BlurText
+              text="Full-Stack Web Developer / Start-Up Focussed Web Expert"
+              delay={200}
+              direction="bottom"
+            />
           </div>
           <div className="font-normal text-md mb-6">
-            Currently working as a Technical Advisor at{" "}
             <a
-              href="https://nwtl.in"
+              href="https://iitr.ac.in"
               rel="noopener noreferer"
               target="_blank"
-              className="underline underline-offset-4"
             >
-              NWTL
+              <BlurText
+                text="IIT Roorkee 2024 Grauate"
+                delay={250}
+                direction="bottom"
+              />
+            </a>
+          </div>
+          <div className="font-normal text-md mb-1">
+            <a href="https://nwtl.in" rel="noopener noreferer" target="_blank">
+              <BlurText
+                text="Currently working as a Technical Advisor at NWTL"
+                delay={300}
+                direction="bottom"
+                className="underline underline-offset-4"
+              />
             </a>
           </div>
         </div>
