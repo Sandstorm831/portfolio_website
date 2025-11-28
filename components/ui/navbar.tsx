8b3d71c64ebe29511c6894f4707556c9223c9975
rohan <gargrohan138@gmail.com>
2025-11-28 15:05:35 +0530

    adding links to the socials

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -6,9 +6,27 @@ export default function Navbar() {
   return (
     <div className="flex md:flex-col md:justify-between text-paper md:h-full max-md:w-full z-10">
       <div className="mt-8 md:mt-12 ml-4 md:ml-8 flex md:flex-col">
-        <CiLinkedin className="max-md:mr-8 md:mb-8 text-2xl" />
-        <FaXTwitter className="max-md:mr-8 md:mb-8 text-2xl" />
-        <FaGithub className="max-md:mr-8 md:mb-8 text-2xl" />
+        <a
+          href="https://www.linkedin.com/in/rohan-garg-1b6b40200/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <CiLinkedin className="max-md:mr-8 md:mb-8 text-2xl cursor-pointer" />
+        </a>
+        <a
+          href="https://x.com/Rodes30947083"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <FaXTwitter className="max-md:mr-8 md:mb-8 text-2xl cursor-pointer" />
+        </a>
+        <a
+          href="https://github.com/Sandstorm831"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <FaGithub className="max-md:mr-8 md:mb-8 text-2xl cursor-pointer" />
+        </a>
         <div className="w-20 h-0 max-md:mt-2.5 md:h-32 md:w-0 md:ml-2.5  border  border-paper" />
       </div>
       <div className="max-md:hidden text-paper rotate-270 mb-12 ml-6 text-sm">
