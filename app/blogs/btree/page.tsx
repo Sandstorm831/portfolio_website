ceecebc2d660f52ae156a8dc005f05d7fddd3d52
rohan <gargrohan138@gmail.com>
2026-03-30 10:28:57 +0530

    a few additions to the blog

--- app/blogs/btree/page.tsx
+++ app/blogs/btree/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import BlogHero from "@/components/ui/blog-hero";
+import BlogImageBlock from "@/components/ui/blog-image-block";
+import BlogSectionHeading from "@/components/ui/blog-section-heading";
+import BlogTextBlock from "@/components/ui/blog-text-block";
 import PageWrapper from "@/components/ui/page-wrapper";
 import BlogsNavbar from "@/components/ui/to-blogs-navbar";
 import { BlogStats } from "@/types";
-import ExcalidrawViewer from "@/components/ExcalidrawWrapper";
 
 const stats: BlogStats = {
   date: new Date("2026-04-18T01:01:01Z"),
@@ -18,13 +20,45 @@ export default function BTree() {
       <BlogsNavbar />
       <div className="flex flex-col h-full w-full overflow-y-scroll no-scrollbar">
         <BlogHero title="B-Tree" stats={stats} />
-        <div>
-          Befor you go on and assume it&apos;s binary tree, I must tell you that
-          there is no full-form for b-trees apart from b-trees itself. Also,
-          B-Tree are very different from binary trees themselves. Here&apos;s a
-          quick look on the structures of both the trees
-        </div>
-        <img src={'/blogs/btree/btree.svg'} />
+        <BlogTextBlock textColor="text-paper">
+          Befor you go on and assume it&apos;s full form as Binary tree, I must
+          tell you that there is no full-form for B-trees apart from B-trees
+          itself. Also, B-Tree are very different from Binary trees themselves.
+          Here&apos;s a quick look on the structures of both the trees.
+        </BlogTextBlock>
+        <BlogImageBlock
+          src="/blogs/btree/btree.svg"
+          alt="Btree-vs-BinaryTree"
+          description="structural differences between a Binary-Tree and a B-Tree"
+        />
+        <BlogTextBlock textColor="text-paper">
+          Now you understand that B-trees are not Binary tree, we can proceed to
+          understand that what are they fundamentally, what is their function
+          and why they are needed at the first place.
+        </BlogTextBlock>
+        <BlogSectionHeading
+          textColor="text-paper"
+          heading="What is B-Trees ?"
+        />
+        <BlogTextBlock textColor="text-paper">
+          Fundamentally, B-Tree is also a data-structure to store data in way
+          that makes it favourable in certain use cases. Here each node stores
+          more than <span className="font-mono">1</span> element and each node
+          points to{" "}
+          <span className="font-mono px-1">
+            1 + (number of elements in that node)
+          </span>{" "}
+          children with root node can be an exception. B-tree is stored in such
+          a manner that all the elements in the child node which is left of an
+          element are smaller than that element and the all the element in the
+          child node which is right to the element is bigger than that element.
+          While elements are themselves sorted in a node.
+        </BlogTextBlock>
+        <BlogImageBlock
+          src="/blogs/btree/btree_struct.svg"
+          alt="structure of btree"
+          description="comparison of elements and children"
+        />
       </div>
     </PageWrapper>
   );
